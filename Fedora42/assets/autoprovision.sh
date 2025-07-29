e5aae1c3ea9873eb208a6169edb6006562d1054d
Gregor Segner <gregor.segner@gmail.com>
2025-07-29 12:31:35 +0200

    feat (Fedora42): adding autoprovision repo uri

--- Fedora42/assets/autoprovision.sh
+++ Fedora42/assets/autoprovision.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-REPO_URL=""
+REPO_URL="https://github.com/g4s/autoprovision.git"
 SKIP_TAGS="vpn,goodnight"
 
 ansible-galaxy role install git+https://github.com/g4s/de.seafi.minimalinstall.git
